329d2d7d1920e7594a360c0f622c92f82d4951d3
Rakin Bhuyan <rabhuyan@uab.edu>
2024-12-21 22:55:02 -0600

    finished Image Generation

--- src/app/api/generate-image/route.ts
+++ src/app/api/generate-image/route.ts
@@ -1,4 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextResponse } from "next/server";
+import { put } from "@vercel/blob";
+import crypto from "crypto";
 
 export async function POST(request: Request) {
   try {
@@ -7,10 +10,45 @@ export async function POST(request: Request) {
 
     // TODO: Call your Image Generation API here
     // For now, we'll just echo back the text
+    console.log(text);
+
+    const apiKey = request.headers.get("X-API-Key");
+
+    if (apiKey !== process.env.API_KEY) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+    const modalURL = process.env.MODAL_URL;
+    const url = new URL(`${modalURL}`);
+
+    url.searchParams.set("prompt", text);
+
+    const response = await fetch(url.toString(), {
+      method: "GET",
+      headers: {
+        "X-API-Key": process.env.API_KEY || "",
+        Accept: "image/jpeg",
+      },
+    });
+
+    if (!response.ok) {
+      const errorText = await response.text();
+      console.error("API Response: ", errorText);
+      throw new Error(
+        `HTTP error! status: ${response.status}, message: ${errorText}`
+      );
+    }
+
+    const imageBuffer = await response.arrayBuffer();
+    const filename = `${crypto.randomUUID()}.jpg`;
+    const blob = await put(filename, imageBuffer, {
+      access: "public",
+      contentType: "image/jpeg",
+    });
 
     return NextResponse.json({
       success: true,
-      message: `Received: ${text}`,
+      imageURL: blob.url,
+      prompt: text,
     });
   } catch (error) {
     return NextResponse.json(
